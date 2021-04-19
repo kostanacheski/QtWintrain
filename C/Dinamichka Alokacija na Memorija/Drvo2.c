4fb78fdadbacd34dffd17989d2a7d355ec9ff2e7
kostanacheski <38078707+kostanacheski@users.noreply.github.com>
2021-04-19 17:43:36 +0200

    Add files via upload

---
+++ C/Dinamichka Alokacija na Memorija/Drvo2.c
@@ -0,0 +1,117 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+int data;
+struct node *left, *right;
+}*root;
+
+struct node *newNode(int item)
+{
+struct node *temp = (struct node *)malloc(sizeof(struct node));
+temp->data = item;
+temp->left = temp->right = NULL;
+return temp;
+}
+
+struct node * minValueNode(struct node* node)
+{
+struct node* current = node;
+
+while (current->left != NULL)
+current = current->left;
+return current;
+}
+
+struct node* delete_node(struct node* root, int data)
+{
+if (root == NULL)
+return root;
+
+if (data < root->data)
+root->left = delete_node(root->left, data);
+
+else if (data > root->data)
+root->right = delete_node(root->right, data);
+else
+{
+
+if (root->left == NULL)
+{
+struct node *temp = root->right;
+free(root);
+return temp;
+}
+else if (root->right == NULL)
+{
+struct node *temp = root->left;
+free(root);
+return temp;
+}
+
+struct node* temp = minValueNode(root->right);
+
+root->data = temp->data;
+
+root->right = delete_node(root->right, temp->data);
+}
+return root;
+}
+
+
+void inorder(struct node *root)
+{
+if (root != NULL)
+{
+inorder(root->left);
+printf("%d ", root->data);
+inorder(root->right);
+}
+}
+
+
+struct node* insert(struct node* node, int data)
+{
+
+if (node == NULL)
+return newNode(data);
+
+if (data < node->data)
+node->left = insert(node->left, data);
+else if (data > node->data)
+node->right = insert(node->right, data);
+return node;
+}
+
+int main()
+{
+int n;
+root = NULL;
+printf("\nVnesi broj na jazli : ");
+scanf("%d", &n);
+int i;
+int data;
+printf("\nVnesi jazli vo drvoto  : ");
+if(n > 0)
+{
+scanf("%d", &data);
+root = insert(root, data);
+}
+for(i = 1; i < n; i++)
+{
+scanf("%d", &data);
+insert(root, data);
+}
+printf("\nPominuvanje vo Inorder na drvoto: ");
+inorder(root);
+printf("\n");
+int del_ele;
+printf("\nVnesi jazel shto treba da se izbrishe : ");
+scanf("%d", &del_ele);
+delete_node(root, del_ele);
+printf("\nPominuvanje vo Inorder posle brishenjeto : ");
+inorder(root);
+printf("\n");
+return 0;
+}
